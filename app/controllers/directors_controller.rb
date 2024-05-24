df94886e4837a131194dda8f506bea5e895152ef
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-24 13:31:28 +0000

    directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,12 @@ class DirectorsController < ApplicationController
 def index
   render({ :template => "director_templates/list"})
 end
+
+  def show
+    params.fetch("the_id")
+
+    @x = Director.where({ :id => the_id })
+
+    render({ :template => "director_templates/details"})
+  end
 end
